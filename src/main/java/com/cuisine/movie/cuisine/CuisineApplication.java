383f5646717a477fcabbcb0b8b7f030fcd88d9ea
coldfish5630 <layan.schicksal@gmail.com>
2024-10-19 09:40:53 +0900

    project init

---
+++ src/main/java/com/cuisine/movie/cuisine/CuisineApplication.java
@@ -0,0 +1,14 @@
+package com.cuisine.movie.cuisine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CuisineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuisineApplication.class, args);
+    }
+
+}
